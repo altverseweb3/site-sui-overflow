6b8c1902ed570994d319fc7eb178d5ed52136c3e
Elmore Shaw <elmore.shaw@furo.io>
2025-02-18 21:08:41 +1100

    chore: fixed up imports

--- site/src/components/layout/MainNav.tsx
+++ site/src/components/layout/MainNav.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter, usePathname } from "next/navigation";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/Button";
 import { TAB_CONFIG } from "@/config/tabs";
 import { Tab } from "@/types/tab";
 
--- site/src/components/layout/SiteHeader.tsx
+++ site/src/components/layout/SiteHeader.tsx
@@ -3,7 +3,7 @@
 import { MainNav } from "@/components/layout/MainNav";
 import Link from "next/link";
 import Image from "next/image";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/Button";
 import { useState } from "react";
 import useWeb3Store from "@/store/web3Store";
 import {
